eb76778d19c6bd636388ee8154eee82a6d26f7f6
Christian Sauer <sauerc@gmail.com>
2014-01-07 12:35:31 -0500

    Importable and refactored getting the data
    Mountains converted so we can import the file and unit test individual
    functions.
    
    Created function to retrieve the data from the file.
    
    Tests: pass
    pep8: pass

--- mountains.py
+++ mountains.py
@@ -1,12 +1,26 @@
+import sys
 from datetime import datetime
 
-now = datetime.now()
-print now.strftime("%Y-%m-%d %H:%M:%S (%A)")
-print ""
 
-lines = [l.rstrip() for l in open("mountains-1.csv", "r")]
-del lines[0]
-for line in lines:
-    data = line.split(",")
-    data[5] = "unknown" if data[5] == "null" else data[5]
-    print "%s has an altitude of %s meters." % (data[1], data[5])
+def get_data(filename):
+    lines = [l.rstrip() for l in open(filename, "r")]
+    return lines
+
+
+def main():
+    now = datetime.now()
+    print now.strftime("%Y-%m-%d %H:%M:%S (%A)")
+    print ""
+
+    lines = get_data("mountains-1.csv")
+    del lines[0]
+    for line in lines:
+        data = line.split(",")
+        data[5] = "unknown" if data[5] == "null" else data[5]
+        print "%s has an altitude of %s meters." % (data[1], data[5])
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- tests.py
+++ tests.py
@@ -1,24 +1,43 @@
-import unittest, subprocess, os
+import unittest
+import subprocess
+import os
+import mountains
+
 
 class CatalystTest(unittest.TestCase):
 
+    def test_get_data(self):
+        filename = "mountains-1.csv"
+
+        expected = self.get_input_file(filename)
+        actual = mountains.get_data(filename)
+
+        self.assert_list(expected, actual)
+
     def test_output(self):
-        expected = self.get_expected()
-        actual = self.get_actual()
+        expected = self.get_expected_output()
+        actual = self.get_actual_output()
 
-        for e, a in zip(expected, actual):
-            self.assertEqual(e, a)
+        self.assert_list(expected, actual)
 
-    def get_expected(self):
-        lines = [l.rstrip() for l in open("mountains-1.expected", "r")]
+    def get_input_file(self, filename):
+        lines = [l.rstrip() for l in open(filename, "r")]
+        return lines
+
+    def get_expected_output(self):
+        lines = self.get_input_file("mountains-1.expected")
         del lines[0]
         return lines
 
-    def get_actual(self):
+    def get_actual_output(self):
         output = subprocess.check_output(["python", "./mountains.py"])
         output_lines = output.split(os.linesep)
         del output_lines[0]
         return output_lines
+
+    def assert_list(self, expected, actual):
+        for e, a in zip(expected, actual):
+            self.assertEqual(e, a)
 
 if __name__ == "__main__":
     unittest.main()
