126a43600d2725ae9d8f55c91422b5ea16116a92
Christian Sauer <sauerc@gmail.com>
2014-01-10 15:30:42 -0500

    script included to generate a large file for testing
    Script will make a 1GB file by copying mountains-1.csv over and over again.  Used to verify that our process will work for large input

---
+++ build_big_file.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main():
+    with open("mountains-1.csv", "r") as f:
+        lines = f.read()
+
+    count = (1024 ** 3) / len(lines)
+
+    with open("mountains-1.big", "w") as f:
+        for x in xrange(count):
+            f.write(lines)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
