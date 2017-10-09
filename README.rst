46b4fd87c1b4e21c1d82559f1327a4ca6a04b88d
Christian Sauer <sauerc@gmail.com>
2017-10-09 19:40:20 -0400

    update README

--- README.rst
+++ README.rst
@@ -18,10 +18,10 @@
 
 .. end-badges
 
-    mountains
-    ~~~~~~~~~
+mountains
+~~~~~~~~~
 
-    Takes a CSV file either via local or HTTP retrieval and outputs information about the mountains according to spec.
+Takes a CSV file either via local or HTTP retrieval and outputs information about the mountains according to spec.
 
-    Originally a programming skills check for a particular position.  I've get it updated to current python versions
-    as well as packaging and testing methodologies.
+Originally a programming skills check for a particular position.  I've get it updated to current python versions
+as well as packaging and testing methodologies.
--- src/mountains/__init__.py
+++ src/mountains/__init__.py
@@ -1,11 +1,11 @@
 """
-    mountains
-    ~~~~~~~~~
+mountains
+~~~~~~~~~
 
-    Takes a CSV file either via local or HTTP retrieval and outputs information about the mountains according to spec.
+Takes a CSV file either via local or HTTP retrieval and outputs information about the mountains according to spec.
 
-    Originally a programming skills check for a particular position.  I've get it updated to current python versions
-    as well as packaging and testing methodologies.
-"""
+Originally a programming skills check for a particular position.  I've get it updated to current python versions
+as well as packaging and testing methodologies.
+"""  # noqa
 from .core import *  # noqa
 from .errors import *  # noqa
