e2f8b59bbd8e6dfeb2dbf7cd504d676039746103
Christian Sauer <sauerc@gmail.com>
2017-10-09 19:42:51 -0400

    update README

--- src/mountains/__init__.py
+++ src/mountains/__init__.py
@@ -6,6 +6,11 @@ Takes a CSV file either via local or HTTP retrieval and outputs information abou
 
 Originally a programming skills check for a particular position.  I've get it updated to current python versions
 as well as packaging and testing methodologies.
+
+
+:copyright: 2016-2017 Christian Erick Sauer.
+:license: MIT, see LICENSE for more details.
+
 """  # noqa
 from .core import *  # noqa
 from .errors import *  # noqa
